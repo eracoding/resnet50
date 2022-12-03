e1defe3323c208c72458d2e244b49714a0ed888c
m_axwel_l <shaxa882@gmail.com>
2022-12-03 23:52:49 +0500

    base directory

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -1,9 +1,13 @@
 from celery import shared_task
 
 from api.search import SearchImage
+import os
+
+from backend.settings import BASE_DIR
 
 
 @shared_task
 def model(category):
     search = SearchImage(category)
-    path_to_image = search.getImage()
+    search.getImage()
+    base = os.path.join(BASE_DIR.absolute(), "images")
